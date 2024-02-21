8377e7bc9696d9075c251bd748c88381cd1bf257
LeNikki <nicolesatembre@gmail.com>
2024-02-21 17:35:18 +0800

    secondary commit

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,9 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.js";
-import About from "./pages/About";
-import Services from "./pages/Services";
-
-function App() {
+import React from "react";
+import logo from "../assets/logo.png";
+export default function Navbar() {
   return (
-    <div className="App">
-      {/* <nav className="w-full bg-transparent flex flex-row p-6 justify-between">
+    <>
+      <nav className="w-full bg-transparent flex flex-row p-6 justify-between">
         <ul className=" w-1/4 flex flex-row justify-between items-center">
           <img src={logo} alt="logo" className="w-12" />
           <li className="hover:font-bold">Home</li>
@@ -24,23 +20,7 @@ function App() {
           </section>
           <i className="fa-solid fa-user ml-5"></i>
         </section>
-      </nav> */}
-
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Home />}>
-            Homes
-          </Route>
-          <Route path="/about" element={<About />}>
-            About
-          </Route>
-          <Route path="/services" element={<Services />}>
-            Services
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+      </nav>
+    </>
   );
 }
-
-export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/navbar";
+//import dogHero from "../assets/dogHero.png";
+function Home() {
+  return (
+    <div className="bg-hero">
+      <Navbar />
+      This is home page
+    </div>
+  );
+}
+
+export default Home;
